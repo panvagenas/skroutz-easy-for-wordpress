fa7d81b794f4a8dc45d3880c57b0834e4ae352a1
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-08-05 12:06:02 +0300

    Initial

---
+++ plugin.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Project: skroutz-easy-for-wordpress
+ * File: plugin.php
+ * User: Panagiotis Vagenas <pan.vagenas@gmail.com>
+ * Date: 5/8/2015
+ * Time: 12:23 πμ
+ * Since: TODO ${VERSION}
+ * Copyright: 2015 Panagiotis Vagenas
+ */
+
+/**
+ * Copyright (C) 2015 Panagiotis Vagenas <pan.vagenas@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* -- WordPress® --------------------------------------------------------------------------------------------------------------------------
+
+Version: 150804
+Stable tag: 150804
+Tested up to: 4.2.4
+Requires at least: 3.5.1
+
+Requires at least Apache version: 2.1
+Tested up to Apache version: 2.4.7
+
+Requires at least PHP version: 5.3.1
+Tested up to PHP version: 5.5.12
+
+Copyright: © 2015 Panagiotis Vagenas <pan.vagenas@gmail.com
+License: GNU General Public License
+Contributors: pan.vagenas
+
+Author: Panagiotis Vagenas <pan.vagenas@gmail.com>
+Author URI: http://gr.linkedin.com/in/panvagenas
+
+Text Domain: skroutz-easy
+Domain Path: /translations
+
+Plugin Name: Skroutz Easy
+Plugin URI: https://github.com/panvagenas/skroutz-easy-for-wordpress
+
+Description: TODO Desscription
+
+Tags: TODO Tags
+
+Kudos: WebSharks™ http://www.websharks-inc.com
+
+-- end section for WordPress®. --------------------------------------------------------------------------------------------------------- */
+
+namespace SkroutzEasy {
+
+	if ( ! defined( 'WPINC' ) ) {
+		die;
+	}
+
+	require_once dirname( __FILE__ ) . '/classes/SkroutzEasy/framework.php';
+}
